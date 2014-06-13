59c0812a79082d9f78d6edb2dad6410371a92feb
gpestana <g6pestana@gmail.com>
2014-06-13 18:02:02 +0200

    room tests

--- rooms.js
+++ rooms.js
@@ -14,13 +14,21 @@ http://yougether.io/watch/<roomID>
 
 function createRoom(url, clbk) {
 	console.log('createRoom')
-	clbk()
+	roomID = null
+	clbk(roomID)
+}
+
+function removeRoom(roomID, clbk) {
+	var err = null
+	console.log('createRoom')
+	clbk(err)
 }
 
 function roomExists(roomID, clbk) {
 	console.log('roomExists')
-	clbk()
+	clbk(true)
 }
+
 
 function addUserToRoom(roomID, userName, clbk) {
 	console.log('addUserToRoom')
@@ -37,9 +45,16 @@ function changeUrlRoom(roomID, url, clbk) {
 	clbk()
 }
 
+function getInfoRoom(roomID, clbk) {
+	var roomInfo = null
+	console.log('get info room')
+	clbk(roomInfo)
+}
 
 exports.createRoom = createRoom
 exports.roomExists = roomExists
+exports.removeRoom = removeRoom
 exports.addUserToRoom = addUserToRoom
 exports.removeUserRoom = removeUserRoom
 exports.changeUrlRoom = changeUrlRoom
+exports.getInfoRoom = getInfoRoom
--- tests/tap_rooms.js
+++ tests/tap_rooms.js
@@ -1,1 +1,46 @@
+var test = require('tape');
 var rooms = require('../rooms.js')
+
+test('createRoom, roomExists and removeRoom', function(t) {
+	t.plan(8)
+
+	var correctRoomID;
+	var wrongRoomID;
+	var urlWrong1 = 'www.youtube.com/'
+	var urlWrong2 = 'www.youtube.com/fssfd'
+	var urlCorrect = 'www.youtube.com/watch?v=2XH5_qafR8k'
+
+	rooms.createRoom(urlWrong1, function(roomID) {
+		t.equal(null, roomID, 'urlWrong2 should not pass')
+	})
+
+	rooms.createRoom(urlWrong2, function(roomID) {
+		t.equal(null, roomID, 'urlWrong2 should not pass')
+		wrongRoomID = roomID
+	})
+
+	rooms.createRoom(urlWrong1, function(roomID) {
+		t.notEqual(null, roomID, 'urlCorrect should pass')
+		correctRoomID = roomID
+	})
+
+	rooms.roomExists(correctRoomID, function(bool) {
+		t.equal(bool, true, 'room should exist')
+	})
+
+	rooms.roomExists(correctRoomID, function(bool) {
+		t.equal(bool, true, 'room should exist')
+	})
+
+	rooms.removeRoom(urlWrong2, function(err) {
+		t.notEqual(err, null, 'room does not exists, error should not be null')
+	})
+
+	rooms.removeRoom(correctRoomID, function(err) {
+		t.equal(err, null, 'room existed before, error should be null')
+	})
+
+	rooms.removeRoom(correctRoomID, function(err) {
+		t.notEqual(err, null, 'room didnt exist when called, error should not be null')
+	})
+})
